7252d8e74ce3922d598c289e9b9a70640a1818b0
Saurabh S <saurabh.satralkar7@gmail.com>
2023-01-21 20:32:28 +0100

    Task creator Typescript

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+import './App.css';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+        <div className='header'>
+              <div className='inputContainer'>
+                  <input type="text" placeholder='Task...' />
+                  <input type="number" placeholder='Deadline (in days)...' />
+              </div>
+              <button> Add Task </button>
+        </div>
+        <div className='todoList'>
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
